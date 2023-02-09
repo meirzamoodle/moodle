1cc2c4403f2be1da9ba62ce315287683b1cc3a7e
Meirza <meirza.arson@moodle.com>
2023-02-09 09:38:11 +0700

    MDL-76411 search: Added class properties that are not declared.
    In PHP 8.2 and later, setting a value to an undeclared class property is
    deprecated and emits a deprecation notice.
    So we need to add missing class properties that still need to be declared.

--- search/engine/simpledb/tests/privacy/provider_test.php
+++ search/engine/simpledb/tests/privacy/provider_test.php
@@ -42,6 +42,33 @@ require_once($CFG->dirroot . '/search/tests/fixtures/mock_search_area.php');
  */
 class provider_test extends \core_privacy\tests\provider_testcase {
 
+    /** @var \search_simpledb\engine */
+    protected $engine;
+
+    /** @var \testable_core_search */
+    protected $search;
+
+    /** @var \core_search_generator */
+    protected $generator;
+
+    /** @var \stdClass course record.*/
+    protected $c1;
+
+    /** @var \stdClass course record.*/
+    protected $c2;
+
+    /** @var \context_course context instance. */
+    protected $c1context;
+
+    /** @var \context_course context instance. */
+    protected $c2context;
+
+    /** @var \stdClass user record. */
+    protected $u1;
+
+    /** @var \stdClass user record. */
+    protected $u2;
+
     public function setUp(): void {
         global $DB;
 
