a56fe7d61c324c1d43ba430805f766b8235ea213
Meirza <meirza.arson@moodle.com>
2023-02-20 07:45:12 +0700

    MDL-77299 question: change $contexts type in docblock.
    Co-authored-by: Tim Hunt <t.j.Hunt@open.ac.uk>

--- question/format.php
+++ question/format.php
@@ -54,7 +54,7 @@ class qformat_default {
     protected $importcontext = null;
     /** @var bool $displayprogress Whether to display progress. */
     public $displayprogress = true;
-    /** @var array Moodle course variable. */
+    /** @var context[] */
     public $contexts;
 
     // functions to indicate import/export functionality
@@ -168,7 +168,7 @@ class qformat_default {
 
     /**
      * set an array of contexts.
-     * @param array $contexts Moodle course variable
+     * @param context[] $contexts
      */
     public function setContexts($contexts) {
         $this->contexts = $contexts;
