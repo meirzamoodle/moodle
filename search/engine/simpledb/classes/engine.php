b11fd0d8ff006941e58a94945fdf1777c8ee0d56
meirzamoodle <meirza.arson@moodle.com>
2024-01-29 16:40:26 +0700

    MDL-78030 search_simpledb: Implementing the count window function

--- search/engine/simpledb/classes/engine.php
+++ search/engine/simpledb/classes/engine.php
@@ -181,10 +181,11 @@ class engine extends \core_search\engine {
         }
 
         // It is limited to $limit, no need to use recordsets.
-        $documents = $DB->get_records_select('search_simpledb_index', implode(' AND ', $ands), $params, 'docid', '*', 0, $limit);
+        $documents = $DB->get_records_select('search_simpledb_index', implode(' AND ', $ands), $params, 'docid', '*', 0, $limit,
+            'fullcount');
 
-        // Hopefully database cached results as this applies the same filters than above.
-        $this->totalresults = $DB->count_records_select('search_simpledb_index', implode(' AND ', $ands), $params);
+        // Get the total data from the documents.
+        $this->totalresults = $documents ? reset($documents)->fullcount : 0;
 
         $numgranted = 0;
 
