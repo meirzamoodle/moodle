81b7d8efbf8589e888f0d1f5b1a247b2312bba0e
Matt Porritt <pozze30@gmail.com>
2024-07-28 20:56:18 +1000

    MDL-80891 AI: Placement Text Editor - Image
    Initial AI placement plugin for the TinyMCE editor.
    It includes a TinyMCE plugin and the AI placement plugin.
    It supports both text and image generation.
    
    Co-authored-by: Huong Nguyen <huongnv13@gmail.com>

---
+++ lib/editor/tiny/plugins/aiplacement/db/access.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tiny text editor AI placement plugin capabilities.
+ *
+ * @package    tiny_aiplacement
+ * @copyright  2024 Huong Nguyen <huongnv13@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    // Capability to generate text using this plugin.
+    'tiny/aiplacement:generatetext' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+        ],
+    ],
+    // Capability to generate image using this plugin.
+    'tiny/aiplacement:generateimage' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+        ],
+    ],
+];
