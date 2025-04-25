cbcef03bfce399ce3ec12f5b34d688b22997a832
meirzamoodle <meirza.arson@moodle.com>
2025-04-25 12:39:50 +0700

    MDL-84832 core: Notify to assistive technology when maxlength is reached

--- lib/db/hooks.php
+++ lib/db/hooks.php
@@ -118,4 +118,8 @@ $callbacks = [
         'hook' => core\hook\output\before_footer_html_generation::class,
         'callback' => [\core\accessibility\live_announcer::class, 'before_footer_html_generation'],
     ],
+    [
+        'hook' => core\hook\output\before_footer_html_generation::class,
+        'callback' => [\core\accessibility\live_announcer_maxlength::class, 'before_footer_html_generation'],
+    ],
 ];
