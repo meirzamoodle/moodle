c31afaba9c5b815363bfab686614bda54d201568
meirzamoodle <meirza.arson@moodle.com>
2025-05-23 13:45:13 +0700

    MDL-85316 dml: Add auroramysql vendor using it's own version

--- lib/dml/auroramysql_native_moodle_database.php
+++ lib/dml/auroramysql_native_moodle_database.php
@@ -42,6 +42,11 @@ class auroramysql_native_moodle_database extends mysqli_native_moodle_database {
     protected $compressedrowformatsupported = false;
 
     /**
+     * String used to identify the Aurora MySQL version in the database driver.
+     */
+    private const AURORA_STRING_VERSION = '.mysql_aurora.';
+
+    /**
      * Returns localised database type name.
      *
      * Returns localised database type name. Can be used before connect().
@@ -92,6 +97,57 @@ class auroramysql_native_moodle_database extends mysqli_native_moodle_database {
         if ($this->external) {
             return parent::transactions_supported();
         }
+        return true;
+    }
+
+    #[\Override]
+    protected function get_version_from_db(): string {
+        $version = null;
+        // Query the DB server for the server version.
+        $sql = "SELECT AURORA_VERSION() aurora_version";
+        try {
+            $result = $this->mysqli->query($sql);
+            if ($result) {
+                if ($row = $result->fetch_assoc()) {
+                    $version = $row['aurora_version'];
+                }
+                $result->close();
+                unset($row);
+            }
+        } catch (\Throwable $e) { // Exceptions in case of MYSQLI_REPORT_STRICT.
+            // It looks like we've an issue out of the expected boolean 'false' result above.
+            throw new dml_read_exception($e->getMessage(), $sql);
+        }
+        if (empty($version)) {
+            // Exception dml_read_exception usually reports raw mysqli errors i.e. not localised by Moodle.
+            throw new dml_read_exception("Unable to read the DB server version.", $sql);
+        }
+
+        return $version;
+    }
+
+    #[\Override]
+    public function get_server_info() {
+        $version = $this->serverversion;
+        if (empty($version)) {
+            $this->serverversion = $this->get_version_from_db();
+        }
+
+        return [
+            'description' => $this->get_mysqli_server_info() . self::AURORA_STRING_VERSION . $this->serverversion,
+            'version' => $this->serverversion,
+        ];
+    }
+
+    #[\Override]
+    protected function is_antelope_file_format_no_more_supported() {
+        // Antelope removed 3.0.0+.
+        return version_compare($this->get_server_info()['version'], '3.0.0', '>=');
+    }
+
+    #[\Override]
+    protected function should_db_version_be_read_from_db(): bool {
+        // Aurora is forced to allow querying the version from the database.
         return true;
     }
 }
